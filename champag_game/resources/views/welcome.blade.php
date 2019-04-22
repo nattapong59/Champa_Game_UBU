de688631f2a981272bc387fba48cbda28a359bc9
jadaphon <jadaphon.ch.59@ubu.ac.th>
2019-04-22 23:27:45 +0700

    cjadd

--- champag_game/resources/views/welcome.blade.php
+++ champag_game/resources/views/welcome.blade.php
@@ -46,8 +46,7 @@
           
         </div><ol class="flex-control-nav flex-control-paging"><li><a class="">1</a></li><li><a class="flex-active">2</a></li></ol><ul class="flex-direction-nav"><li class="flex-nav-prev"><a class="flex-prev" href="#"><em class="fa fa-angle-left"></em></a></li><li class="flex-nav-next"><a class="flex-next" href="#"><em class="fa fa-angle-right"></em></a></li></ul></div>
 
-<<<<<<< HEAD
-=======
+
 
 
 </div>
@@ -63,7 +62,7 @@
 
 
 <br>
->>>>>>> 6b84824e1c5ecef0f970a9d5ef557be8b12ce4ac
+
 <div class="ec-main-content">
     <div class="ec-main-section ec-promofull">
         <div class="container">
